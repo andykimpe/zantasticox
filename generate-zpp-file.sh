fb7adb518e6a70369d4134ea375d551e6539c900
Kimpe Andy <andykimpe@gmail.com>
2013-12-15 17:04:15 +0100

    Update generate-zpp-file.sh

--- generate-zpp-file.sh
+++ generate-zpp-file.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+## download git and create environnement
+cd /root
+git clone https://github.com/andykimpe/zantasticotx.git
+cd zantasticotx
+rm -rf .git .gitignore
+mkdir packages
+cd packages
+
+## generate xml file and create packages
